3eb12d330d7b0cdc57438953b4fab5979758a35f
Griffin Barrett <griffin-barrett@outlook.com>
2021-03-30 18:15:56 -0400

    removed erant print and added a dll line to .gitignore

--- src/p_focal.cpp
+++ src/p_focal.cpp
@@ -29,11 +29,9 @@ Rcpp::List openmp_self_test_cpp() {
 Rcpp::NumericMatrix p_focal_cpp(const Rcpp::NumericMatrix& data, const Rcpp::NumericMatrix& kernel, const double default_value, const size_t transform_fun, const size_t reduce_fun, const bool open_mp){
     TRANSFORM tf((TRANSFORM)transform_fun);
     REDUCE rf((REDUCE)reduce_fun);
-    
-    std::cerr << "asdf\n";
-    
+
     bool good = true;
-    
+
     if(tf >= TRANSFORM::SIZE){
         std::cerr << "\n"
                   << "The transform function, ie: 'weight_fun', is not a valid value.\n"
@@ -46,19 +44,19 @@ Rcpp::NumericMatrix p_focal_cpp(const Rcpp::NumericMatrix& data, const Rcpp::Num
                   << "It is " << ((size_t)rf) << " when it must be in the range [0, " << ((size_t)REDUCE::SIZE) << ")\n";
         good = false;
     }
-    
+
     if(!good){
         return{0};
     }else{
         //std::cout << ((size_t)tf) << ", " << ((size_t)rf) << "\n";
-        
+
         expanded_aligned_data<> src(&data[0], data.ncol(), data.nrow(), kernel.ncol()/2, kernel.nrow()/2, default_value);
         expanded_aligned_data<> k(&kernel[0], kernel.ncol(), kernel.nrow(), 0, 0, 0);
-        
+
         Rcpp::NumericMatrix dest(data.nrow(), data.ncol());
-        
+
         p_focal_switch_wall(src, k, &dest[0], tf, rf, open_mp);
-        
+
         return dest;
     }
 }
