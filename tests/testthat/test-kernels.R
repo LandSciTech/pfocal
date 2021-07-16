0c89eec51a500515c9b08360c87202ed92dfd87f
Valentin <valentin.lucet@gmail.com>
2021-07-16 11:46:04 -0400

    add more tests for circular kernels-some cases not covered

--- tests/testthat/test-kernels.R
+++ tests/testthat/test-kernels.R
@@ -1,4 +1,4 @@
-test_that("distance kernels work as expcted", {
+test_that("distance kernels work as expected", {
   
   expect_equal(minkowski_distance_kernel(p = 2, vertical_radius = 2, horizontal_radius = 2), 
                matrix(c(2.828427, 2.236068, 2, 2.236068, 2.828427,
@@ -51,4 +51,40 @@ test_that("distance kernels work as expcted", {
   
   expect_error(vertical_distance_kernel(vertical_radius = -1, horizontal_radius = 2),
                "The radius must be >= 0")
+})
+
+test_that("circle kernels work as expected", {
+  
+  r <- 2
+  
+  hard <- matrix(c(0, 0, 1, 0, 0,
+                   0, 1, 1, 1, 0,
+                   1, 1, 1, 1, 1,
+                   0, 1, 1, 1, 0,
+                   0, 0, 1, 0, 0), nrow = 5, ncol = 5, byrow = TRUE)
+  
+  smooth <- matrix(c(0.0000000, 0.2138283, 0.4789669, 0.2138283, 0.0000000,
+                     0.2138283, 0.9849691, 1.0000000, 0.9849691, 0.2138283,
+                     0.4789669, 1.0000000, 1.0000000, 1.0000000, 0.4789669,
+                     0.2138283, 0.9849691, 1.0000000, 0.9849691, 0.2138283,
+                     0.0000000, 0.2138283, 0.4789669, 0.2138283, 0.0000000), 
+                   nrow = 5, ncol = 5, byrow = TRUE)
+  
+  expect_equal(hard_uniform_circle_kernel(r), hard, tolerance = 1e-7)
+  expect_equal(smooth_uniform_circle_kernel(r), smooth, tolerance = 1e-7)
+  
+  expect_equal(sum(hard_uniform_circle_kernel(r)), 13)
+  expect_equal(sum(smooth_uniform_circle_kernel(r)), 12.5663706)
+  
+  smooth_one_half <- matrix(c(0, 0.0000000, 0,
+                              0, 0.7853982, 0,
+                              0, 0.0000000, 0), 
+                            nrow = 3, ncol = 3, byrow = TRUE)
+  expect_equal(smooth_uniform_circle_kernel(1/2), smooth_one_half, tolerance = 1e-7)
+  
+  smooth_under_sqrtof2 <- matrix(c(0.0000000, 0.1348958, 0.0000000,
+                                   0.1348958, 0.9997973, 0.1348958,
+                                   0.0000000, 0.1348958, 0.0000000), 
+                                 nrow = 3, ncol = 3, byrow = TRUE)
+  expect_equal(smooth_uniform_circle_kernel(0.7), smooth_under_sqrtof2, tolerance = 1e-7)
 })
