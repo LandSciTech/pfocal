b8e4191488a71b38441fc93be8e536d6d767c840
Valentin <valentin.lucet@gmail.com>
2021-12-10 16:49:33 -0500

    fix test

--- tests/testthat/test-pfocal_fast.R
+++ tests/testthat/test-pfocal_fast.R
@@ -4,11 +4,11 @@ base_image[10,10] <- NA
 
 test_that("emulating mean works", {
   # TODO best tests are needed here
-  pfocal_fast_gaussian_radius(data, vertical_radius = 2)
-  pfocal_fast_gaussian_confidence(data)
-  pfocal_fast_binomial(data, vertical_radius = 2)
-  pfocal_fast_abs_rectangle(data, height = 2)
-  pfocal_fast_separated(data,
+  pfocal_fast_gaussian_radius(base_image, vertical_radius = 2)
+  pfocal_fast_gaussian_confidence(base_image)
+  pfocal_fast_binomial(base_image, vertical_radius = 2)
+  pfocal_fast_abs_rectangle(base_image, height = 2)
+  pfocal_fast_separated(base_image,
                         kernel_list = list(binomial_kernel(vertical_radius = 2,
                                                            horizontal_radius = 2),
                                            distance_kernel(vertical_radius = 2,
